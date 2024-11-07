69726a3c4304ac1ca1d558a23d953a025085a2b9
Sadri <108393507+fragmetal@users.noreply.github.com>
2024-11-07 20:39:11 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /*Power*/
 module.exports = {
-    botMode: 'live', // live or tester
+    botMode: 'tester', // live or tester
     botNode: 'localnode', // prodnode or localnode
     mongoURI: `mongodb+srv://sadri:sadri@cluster0.eyfridx.mongodb.net/`,
     dbName: `Relay`
